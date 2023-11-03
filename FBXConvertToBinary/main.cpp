dc8ac626207e55f93c50606d4f40fc6348653155
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2023-11-03 14:19:38 +0900

    FBXファイルの解析結果をBinaryファイルに出力するプログラムを作成した。10/31より開始したが、git管理でミスしたため新たにリポジトリを作成して結果のみプッシュする。

---
+++ FBXConvertToBinary/main.cpp
@@ -0,0 +1,19 @@
+#include <FBXInfoManager.h>
+
+int main() {
+	std::vector< std::string> modelPath;
+	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\BattleField_Test.fbx");
+	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\Boxs_diagonal.fbx"); 
+	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\Box_diagonal_lpos.fbx"); 
+	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\Box_diagonal.fbx"); 
+	// 3 texture model
+	modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\ancient\\ziggurat_test2.fbx");
+
+	// 4 textures model
+	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\NewConnan\\NewConnan2.fbx");
+	modelPath.push_back("C:\\Users\\RyoTaka\\Documents\\RenderingDemoRebuild\\FBX\\NewConnan_ZDir.fbx");
+
+	auto fbxInfoManager = FBXInfoManager::Instance();
+	fbxInfoManager.Init(modelPath[0]);
+	return 0;
+}
